8e5ab10d8ef1193ab123971ef08cd2440a75710a
Cobbmanu <cmaniev@gmail.com>
2021-03-12 19:08:14 +0600

    5 commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import './About.css';
 import { Col, Container, Image, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
-import Profile from "../../assets/img/profile/profile2.png";
+import Profile from "../../assets/img/profile/profile3.png";
 
 
 const About = () => {
